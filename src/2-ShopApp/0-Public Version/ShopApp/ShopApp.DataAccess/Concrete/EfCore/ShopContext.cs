acb7a3c38adbccf5cbcef80dbf56fef99c5b6a52
cemsayin <51953047+cemsayin@users.noreply.github.com>
2022-02-09 01:51:51 -0800

    You dont need to add PK from here. You can use KEY data annotation on Entity Model
    https://docs.microsoft.com/en-us/ef/ef6/modeling/code-first/data-annotations#key

--- src/2-ShopApp/0-Public Version/ShopApp/ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
+++ src/2-ShopApp/0-Public Version/ShopApp/ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
@@ -12,12 +12,7 @@ namespace ShopApp.DataAccess.Concrete.EfCore
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ShopDb;integrated security=true;");
         }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<ProductCategory>()
-                    .HasKey(c => new { c.CategoryId, c.ProductId });
-        }
+        
         public DbSet<Product> Products{ get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Cart> Carts { get; set; }
